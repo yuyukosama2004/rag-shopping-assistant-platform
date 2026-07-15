8e26799f3aa29d54dba75e1f77171dd14b1a5531
yuyukosama2004 <yoyokosama2004@gmail.com>
2026-07-15 14:16:58 +0800

    refactor: remove unnecessary service registry

--- user-service/src/main/java/com/biyesheji/user/UserApplication.java
+++ user-service/src/main/java/com/biyesheji/user/UserApplication.java
@@ -2,10 +2,8 @@ package com.biyesheji.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.biyesheji")
-@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
